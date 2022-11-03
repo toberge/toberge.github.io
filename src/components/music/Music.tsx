82622585b66829dd608cd20f6544b82db879a435
Tore Bergebakken <toberge@stud.ntnu.no>
2022-11-03 14:53:46 +0100

    Add some recent music

--- src/components/music/Music.tsx
+++ src/components/music/Music.tsx
@@ -12,10 +12,21 @@ const Music = () => {
             </h1>
             <ul className="music-area">
                 {[
-                    "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/420930108",
+                    // Woodland Chase
+                    "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1368671695",
+                    // Velvet Touch
                     "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/455028900",
+                    // The Missing Floor
+                    "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/420930108",
+                    // Small Steps
+                    "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1355510512",
+                    // Lethargic Raindrops
+                    "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1301526613",
+                    // Blue Bird
                     "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1006273291",
+                    // Clavelian March
                     "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/150459813",
+                    // Obsidian Alley
                     "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/173644457",
                 ].map(url => (
                     <li className="project">
